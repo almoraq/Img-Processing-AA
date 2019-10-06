e95c7a0d98a8ea5670a9197ed8f24db2e0577572
almoraq <55812986+almoraq@users.noreply.github.com>
2019-10-05 23:02:57 -0600

    Population generation function added
    Child created from 2 polygons from matingPool, with optimization to find "better parents"

--- src/main/java/geneticalgorithm/PolygonDNA.java
+++ src/main/java/geneticalgorithm/PolygonDNA.java
@@ -10,5 +10,48 @@ package geneticalgorithm;
  * @author amora
  */
 public class PolygonDNA {
+    int redValue;
+    int greenValue;
+    int blueValue;
+    byte colorValueBytes[];
+    
+    public PolygonDNA(int pRedValue, int pGreenValue, int pBlueValue){
+        this.redValue = pRedValue;
+        this.greenValue = pGreenValue;
+        this.blueValue = pBlueValue;
+        colorValueBytes = new byte[3];
+        byte redValueByte = (byte)(this.redValue-128);
+        byte greenValueByte = (byte)(this.greenValue-128);
+        byte blueValueByte = (byte)(this.blueValue-128);
+        colorValueBytes[0] = redValueByte;
+        colorValueBytes[1] = greenValueByte;
+        colorValueBytes[2] = blueValueByte;
+        System.out.println("Red byte: "+redValueByte+"\tGreen byte: "+greenValueByte+"\tBlue byte: "+blueValueByte);
+        
+    }
+
+    public int getRedValue() {
+        return redValue;
+    }
+
+    public void setRedValue(int redValue) {
+        this.redValue = redValue;
+    }
+
+    public int getGreenValue() {
+        return greenValue;
+    }
+
+    public void setGreenValue(int greenValue) {
+        this.greenValue = greenValue;
+    }
+
+    public int getBlueValue() {
+        return blueValue;
+    }
+
+    public void setBlueValue(int blueValue) {
+        this.blueValue = blueValue;
+    }
     
 }
